f36ba2eb5d88d920632594abf2b94e75c209ff08
andronopoulos <vasileiosandronopoulos@gmail.com>
2025-07-22 16:31:20 +0300

    There was an issue

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$8q-3qv-7dd93886gch+k_(*2rol$kl@)e^+9+ogl+b@tkrei0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['Andronopoulos2003.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
